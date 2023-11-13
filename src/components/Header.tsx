7caebd4d8ede90bacc5e8d9edb3f63876aa70bb9
HamSangeok <hamsoo159@naver.com>
2023-11-14 00:27:43 +0900

    home update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,31 +4,27 @@ import { faUser } from '@fortawesome/free-regular-svg-icons';
 
 import {Link, useLocation} from "react-router-dom";
 
-interface RouteState {
-    name : string;
-}
 
 export default function Header() {
     const location = useLocation();
-    const state = location.state as RouteState;
+    const pathSegments:string[] = location.pathname.split('/');
+    const path:string = `/${pathSegments[1]}`;
 
     return (
         <div className="flex">
-            {/* {
-                console.log(state);
-            } */}
             <div className="flex w-screen justify-between padding p-10">
                 <div className="flex">
                     <div className="text-stone-900 text-base font-normal font-['Asar']"><Link to="/">CAMPSHARE</Link></div>
                     <div className="flex pl-14">
                     <div className="w-[372px] h-[27px] justify-start items-start gap-[50px] inline-flex">
-                        <div className="text-stone-900 text-opacity-50 text-base font-medium font-['Poppins']"><Link to="/">Home</Link></div>
-                        <div className="text-stone-900 text-opacity-50 text-base font-medium font-['Poppins']"><Link to="/categories">Categories</Link></div>
-                        <div className="text-stone-900 text-opacity-50 text-base font-medium font-['Poppins']"><Link to="/recommend">Recommend</Link></div>
+                        <div className={`text-stone-900 text-base font-medium font-['Poppins'] ${path === "/" ? "font-bold" : "text-opacity-50"}`}><Link to="/">Home</Link></div>
+                        <div className={`text-stone-900 text-base font-medium font-['Poppins'] ${path === "/categories" ? "font-bold" : "text-opacity-50"}`}><Link to="/categories">Categories</Link></div>
+                        <div className={`text-stone-900 text-base font-medium font-['Poppins'] ${path === "/recommend" ? "font-bold" : "text-opacity-50"}`}><Link to="/recommend">Recommend</Link></div>
                         </div>
                     </div>
                 </div>
                 <div className="w-[71px] h-6 justify-start items-center gap-[23px] inline-flex">
+                    {/* 여기도 Link로 바꿔줘야 함 */}
                     <div className="w-6 h-6 relative"><FontAwesomeIcon icon={faHeart} /></div>
                     <div className="w-6 h-6 relative"><FontAwesomeIcon icon={faUser} /></div>
                 </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
     return (
-        <div>
-            Home
+        <div className="flex min-w-screen min-h-screen px-10">
+            <div className="flex flex-col justify-between min-w-[100%] min-h-[50%] rounded-3xl bg-[url('./assets/picture1.jpg')] bg-cover text-white ">
+                <div/>
+                <div className="flex flex-col align-middle pt-20 pl-20">
+                    <div className="text-5xl">
+                        CampShare
+                    </div>
+                    <div className="text-2xl pt-8">
+                        캠핑, 부담은 감소하고 즐거움은 증가하다.
+                    </div>
+                </div>
+                <div/>
+            </div>
         </div>
     )
 }
